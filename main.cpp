d6fb4357fcf47774033bd72129f4312d6b8e42c7
Daniel Sawyer <sawyermade@gmail.com>
2016-04-21 19:50:35 -0400

    FUCK IT

--- main.cpp
+++ main.cpp
@@ -14,12 +14,9 @@ int main()
 	//int nr_states = 0;
 
 	cout << "This is the Level 0 Ticket Booth program!" << endl;
-	//int nr_venues = 3;
-	/*Venue* venue = Venue_From_XML::Get_Venue_From_XML();
-	venue->Display_All();*/
 
-	Venue_From_XML fuckit;
-	fuckit.Get_Venue_From_XML();
+	Venue_From_XML ven;
+	ven.Get_Venue_From_XML();
 	// Venue** venues = fuckit.venues();
 
 
@@ -27,13 +24,13 @@ int main()
 	// int nr_venues = fuckit.size();
 	//int num_states = nr_venues;
 	
-	cout << endl << "num of elem " << *fuckit.size() << endl;
+	//cout << endl << "num of elem " << *fuckit.size() << endl;
 	/*for (int i = 0; i < 3; ++i)
 	{
 		venues[i]->Display_All();
 		//nr_venues++;
 	}*/
-	Command_Processor::Process_Commands(fuckit.venues(), fuckit.size());
+	Command_Processor::Process_Commands(ven.venues(), ven.size());
 
 	cout << "\nNormal Termination ";
 	//cin.get();
