d470e44097ca07b1126e5bba4111d882fda7c48b
Daniel Sawyer <sawyermade@gmail.com>
2016-04-21 19:54:19 -0400

    FUC KIT

--- main.cpp
+++ main.cpp
@@ -10,26 +10,12 @@ using namespace std;
 
 int main()
 {
-	//Venue* venues[999];
-	//int nr_states = 0;
 
-	cout << "This is the Level 0 Ticket Booth program!" << endl;
+	cout << "This is the Level 1 Ticket Booth program!" << endl;
 
 	Venue_From_XML ven;
 	ven.Get_Venue_From_XML();
-	// Venue** venues = fuckit.venues();
-
-
-
-	// int nr_venues = fuckit.size();
-	//int num_states = nr_venues;
 	
-	//cout << endl << "num of elem " << *fuckit.size() << endl;
-	/*for (int i = 0; i < 3; ++i)
-	{
-		venues[i]->Display_All();
-		//nr_venues++;
-	}*/
 	Command_Processor::Process_Commands(ven.venues(), ven.size());
 
 	cout << "\nNormal Termination ";
