320b576b1163b258055d9ed9b5d476c7b4336019
alando1 <arodriguez7@mail.usf.edu>
2016-04-21 19:38:24 -0400

    Added files via upload

--- main.cpp
+++ main.cpp
@@ -3,16 +3,39 @@
 #include "Venue.h"
 #include "Venue_From_User.h"
 #include "Venue_From_XML.h"
+#include "menu.h"
+#include "Command_Processor.h"
 
 using namespace std;
 
 int main()
 {
+	//Venue* venues[999];
+	//int nr_states = 0;
+
 	cout << "This is the Level 0 Ticket Booth program!" << endl;
+	//int nr_venues = 3;
+	/*Venue* venue = Venue_From_XML::Get_Venue_From_XML();
+	venue->Display_All();*/
 
-	Venue* venue = Venue_From_XML::Get_Venue_From_XML();
-	venue->Display_All();
+	Venue_From_XML fuckit;
+	fuckit.Get_Venue_From_XML();
+	// Venue** venues = fuckit.venues();
 
-	cin.get();
+
+
+	// int nr_venues = fuckit.size();
+	//int num_states = nr_venues;
+	
+	cout << endl << "num of elem " << *fuckit.size() << endl;
+	/*for (int i = 0; i < 3; ++i)
+	{
+		venues[i]->Display_All();
+		//nr_venues++;
+	}*/
+	Command_Processor::Process_Commands(fuckit.venues(), fuckit.size());
+
+	cout << "\nNormal Termination ";
+	//cin.get();
 	return 0;
 }
